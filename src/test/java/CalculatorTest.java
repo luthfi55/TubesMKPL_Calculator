c332e6c39a9ea8348a7c01d002795ae6648170e5
luthfi55 <49300990+luthfi55@users.noreply.github.com>
2023-06-11 15:10:52 +0700

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,35 +1,37 @@
-import junit.framework.TestCase;
+package src.test.java;
 
-public class CalculatorTest extends TestCase{
-    public static void main(String[] args) {
-        testAddition();
-        testSubtraction();
-        testMultiplication();
-        testDivision();
-        testComplexExpression();
-    }
+import static org.junit.Assert.*;
+import src.main.java.*;
+import org.junit.Test;
 
+public class CalculatorTest{
+    
+    @Test
     public static void testAddition() {
         String expression = "2 + 3";
         double expected = 5.0;
         double result = Double.parseDouble(Calculator.calculateExpression(expression));
         System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
     }
-
+    
+    
+    @Test
     public static void testSubtraction() {
         String expression = "5 - 3";
         double expected = 2.0;
         double result = Double.parseDouble(Calculator.calculateExpression(expression));
         System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
     }
-
+    
+    @Test
     public static void testMultiplication() {
         String expression = "4 * 3";
         double expected = 12.0;
         double result = Double.parseDouble(Calculator.calculateExpression(expression));
         System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
     }
-
+    
+    @Test
     public static void testDivision() {
         String expression = "10 / 2";
         double expected = 5.0;
@@ -37,6 +39,7 @@ public class CalculatorTest extends TestCase{
         System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
     }
 
+    @Test
     public static void testComplexExpression() {
         String expression = "2 + 3 * (4 - 1)";
         double expected = 11.0;
