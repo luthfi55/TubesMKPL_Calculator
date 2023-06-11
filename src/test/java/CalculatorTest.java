fa93b805397153aa2df05e63f10993ad56e8f4b7
luthfi55 <49300990+luthfi55@users.noreply.github.com>
2023-06-11 15:15:47 +0700

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,49 +1,44 @@
-package src.test.java;
-
-import static org.junit.Assert.*;
-import src.main.java.*;
 import org.junit.Test;
 
 public class CalculatorTest{
-    
+
     @Test
-    public static void testAddition() {
+    public void testAddition() {
         String expression = "2 + 3";
         double expected = 5.0;
         double result = Double.parseDouble(Calculator.calculateExpression(expression));
-        System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
-    }
-    
-    
-    @Test
-    public static void testSubtraction() {
-        String expression = "5 - 3";
-        double expected = 2.0;
-        double result = Double.parseDouble(Calculator.calculateExpression(expression));
-        System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
-    }
-    
-    @Test
-    public static void testMultiplication() {
-        String expression = "4 * 3";
-        double expected = 12.0;
-        double result = Double.parseDouble(Calculator.calculateExpression(expression));
-        System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
-    }
-    
-    @Test
-    public static void testDivision() {
-        String expression = "10 / 2";
-        double expected = 5.0;
-        double result = Double.parseDouble(Calculator.calculateExpression(expression));
-        System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
+        org.junit.Assert.assertEquals(expression + " = " + result, expected, result, 0.0);
     }
 
     @Test
-    public static void testComplexExpression() {
+    public void testSubtraction() {
+        String expression = "5 - 3";
+        double expected = 2.0;
+        double result = Double.parseDouble(Calculator.calculateExpression(expression));
+        org.junit.Assert.assertEquals(expression + " = " + result, expected, result, 0.0);
+    }
+
+    @Test
+    public void testMultiplication() {
+        String expression = "4 * 3";
+        double expected = 12.0;
+        double result = Double.parseDouble(Calculator.calculateExpression(expression));
+        org.junit.Assert.assertEquals(expression + " = " + result, expected, result, 0.0);
+    }
+
+    @Test
+    public void testDivision() {
+        String expression = "10 / 2";
+        double expected = 5.0;
+        double result = Double.parseDouble(Calculator.calculateExpression(expression));
+        org.junit.Assert.assertEquals(expression + " = " + result, expected, result, 0.0);
+    }
+
+    @Test
+    public void testComplexExpression() {
         String expression = "2 + 3 * (4 - 1)";
         double expected = 11.0;
         double result = Double.parseDouble(Calculator.calculateExpression(expression));
-        System.out.println(expression + " = " + result + " (Expected: " + expected + ")");
+        org.junit.Assert.assertEquals(expression + " = " + result, expected, result, 0.0);
     }
 }
